478593627d7b7eb88e0c014cd2f13a98f144dbcc
odnosumAnna <162055933+odnosumAnna@users.noreply.github.com>
2024-09-28 21:57:12 +0300

    commit

--- 2.rb
+++ 2.rb
@@ -20,15 +20,40 @@ class RPN
     ['+', '-', '*', '/'].include?(char)
   end
 
+  def divide_by_zero?(tokens)
+    tokens.each_with_index do |token, index|
+      if token == '/' && tokens[index + 1] == '0'
+        return true
+      end
+    end
+    false
+  end
+
   def to_rpn(expression)
     operators = []
     output = []
 
-    tokens = expression.split
+    #  зайві оператори
+    if expression.strip[-1] =~ /[+\-*\/]/
+      raise "Помилка: вираз не може закінчуватися оператором."
+    end
 
-    tokens.each do |token|
-      if token =~ /\d+/
+    tokens = expression.gsub('(', ' ( ').gsub(')', ' ) ').split
+
+    if divide_by_zero?(tokens)
+      raise "Помилка: ділення на нуль."
+    end
+
+    tokens.each_with_index do |token, index|
+      if token =~ /-?\d+(\.\d+)?/  # для дробових і від'ємних чисел
         output.push(token)
+      elsif token == '('
+        operators.push(token)
+      elsif token == ')'
+        while operators.any? && operators.last != '('
+          output.push(operators.pop)
+        end
+        operators.pop
       elsif operator?(token)
         while operators.any? && precedence(operators.last) >= precedence(token)
           output.push(operators.pop)
@@ -40,11 +65,18 @@ class RPN
     while operators.any?
       output.push(operators.pop)
     end
+
     output.join(' ')
   end
 end
-# Приклад за умовою
+
 converter = RPN.new
-expression = "2 + 1 * 4"
-rpn_result = converter.to_rpn(expression)
-puts rpn_result
+
+# тестування
+begin
+  expression = "4 - 5 + (-4 + 5)"
+  rpn_result = converter.to_rpn(expression)
+  puts "RPN вираз: #{rpn_result}"
+rescue => e
+  puts e.message
+end
